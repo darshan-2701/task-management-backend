964637db89886b4ee1314487c522fc974ba04e62
darshan-2701 <darshangowda9484@gmail.com>
2025-03-19 22:50:20 +0530

    fix

--- utils/dbutils.py
+++ utils/dbutils.py
@@ -1,5 +1,7 @@
-db_host = "localhost"
-db_port = 5432
-db_name = 'task_management'
-db_user = 'postgres'
-db_password = 'password'
+import os
+
+db_host = os.getenv('DB_HOST', 'localhost')
+db_port = os.getenv('DB_PORT', 5432)
+db_name = os.getenv('DB_NAME', 'task_management')
+db_user = os.getenv('DB_USER', 'postgres')
+db_password = os.getenv('DB_PASSWORD', 'password')
